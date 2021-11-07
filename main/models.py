87d1971ccba5296ef263884dfd196342afd13073
Murager <jmmukanov@gmail.com>
2021-11-07 15:56:09 +0600

    add recipe logic

--- main/models.py
+++ main/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 User = get_user_model()
 
 
 class Recipe(models.Model):
     checked             = models.BooleanField(default=False)
+    author              = models.ForeignKey(User, default=1, verbose_name="Автор", on_delete=models.CASCADE)
     title               = models.CharField(max_length=255)
     slug                = models.SlugField(unique=True, verbose_name='URL', null=True)
     cooking             = RichTextField(blank=True, null=True)
@@ -16,3 +18,6 @@ class Recipe(models.Model):
     created_at          = models.DateTimeField(auto_now_add=True, editable=False)
     likes               = models.ManyToManyField(User, blank=True, related_name='Likes')
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Recipe, self).save(*args, **kwargs)
